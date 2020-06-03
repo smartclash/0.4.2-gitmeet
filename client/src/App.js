a5a7b84cee6ffb0ecb8b70458e093c66a611934f
Kartik Choudhary <kartik.c918@gmail.com>
2020-06-03 12:28:27 +0530

    WIP Header Design, add new graphic, update deps

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <ZeitProvider>      // ---> Base provider
-    <CssBaseline />     // ---> normalize styles
+    <ZeitProvider>
+    <CssBaseline />
       <Header />
       <p>
         GitMeet!
