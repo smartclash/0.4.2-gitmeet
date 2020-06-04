965b58d0b5eae7b3fd04979991b8dab8de7b3bd0
Alpha Man <xalphamanx@tuta.io>
2020-06-05 03:04:51 +0530

    Added header support

--- server/src/App.ts
+++ server/src/App.ts
@@ -13,7 +13,7 @@ Mongoose.connection.on('open', () => console.log('Connected to database'));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 app.use(express.json());
